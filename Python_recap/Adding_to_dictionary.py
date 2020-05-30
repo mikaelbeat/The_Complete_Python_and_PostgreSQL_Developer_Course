c7317673a3d4700b18e11427337a8417085cec98
mikaelbeat <ryynanenphm@gmail.com>
2020-05-30 13:47:12 +0300

    Update

--- Python_recap/Adding_to_dictionary.py
+++ Python_recap/Adding_to_dictionary.py
@@ -1,7 +1,12 @@
 
 friends = {"Rolf": 34, "Anne": 27}
 
+# Adding to dictionary works sameway than updating
 friends["Adam"] = 30
 friends["Rolf"] = 37
 
 print(friends)
+
+
+for friend, age in friends.items():
+    print(f"{friend} - {age}")
---
+++ Python_recap/Lambda_functions.py
@@ -0,0 +1,7 @@
+
+# lambda x, y: x + y
+
+add = lambda x, y: x + y
+
+print(add(5, 7))
+
