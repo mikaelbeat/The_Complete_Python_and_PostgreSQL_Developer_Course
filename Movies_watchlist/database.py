b6ba3040ec4c0769f286c6b5642bcd747cde3639
mikaelbeat <ryynanenphm@gmail.com>
2020-06-30 23:10:46 +0300

    Update

--- Movies_watchlist/database.py
+++ Movies_watchlist/database.py
@@ -12,11 +12,12 @@ INSERT_MOVIES = "INSERT INTO movies (title, release_timestamp, watched) VALUES (
 SELECT_ALL_MOVIES = "SELECT * FROM movies;"
 SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
 SELECT_WATCHED_MOVIES = "SELECT * FROM movies WHERE watched = 1;"
+SET_MOVIE_WATCHED = "UPDATE movies SET watched = 1 WHERE title = ?;"
 
 connection = sqlite3.connect("data.db")
 
 
-def create_table():
+def create_tables():
     with connection:
         connection.execute(CREATE_MOVIES_TABLE)
 
@@ -35,7 +36,8 @@ def get_movies(upcoming=False):
         return cursor.fetchall()
 
 def watch_movie(title):
-    pass
+    with connection:
+        connection.execute(SET_MOVIE_WATCHED, (title,))
 
 def get_watched_movies():
     with connection:
